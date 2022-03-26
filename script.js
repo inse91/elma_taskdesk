6036274d4ed233c4a123fd936f6ce5a42a27de09
inse91 <insenta@gmail.com>
2022-03-26 11:12:39 +0300

    fix names

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const CalLen = 8; //число столбцов
-const UsersURL =
+const USERSURL =
   "https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/users";
-const TasksURL =
+const TASKSURL =
   "https://varankin_dev.elma365.ru/api/extensions/2a38760e-083a-4dd0-aebc-78b570bfd3c7/script/tasks";
-
-const Users = getDataFromURL(UsersURL);
-const Tasks = getDataFromURL(TasksURL);
-
-const backlog = [];
-
-const userMap = new Map();
-const taskMap = new Map();
+const USERS = getDataFromURL(USERSURL);
+const TASKS = getDataFromURL(TASKSURL);
+const BACKLOG = [];
+const USERMAP = new Map();
+const CALENDARSIZE = 8; //число столбцов
 
 let weekCounter = 0;
 
@@ -27,11 +23,11 @@ function getDataFromURL(url) {
   }
 }
 
-function shiftCalFwd() {
+function shiftCalendarForwards() {
   weekCounter++;
   createCal(weekCounter);
 }
-function shiftCalBckwd() {
+function shiftCalendarBackwards() {
   weekCounter--;
   createCal(weekCounter);
 }
@@ -51,10 +47,10 @@ function getHeaderDate(date) {
 function getDateArray() {
   let dateArray = [""];
   let today = new Date();
-  for (let k = 0; k < CalLen; k++) {
-    today.setDate(today.getDate() - today.getDay() + k + 7 * weekCounter);
+  for (let i = 0; i < CALENDARSIZE; i++) {
+    today.setDate(today.getDate() - today.getDay() + i + 7 * weekCounter);
 
-    if (k !== 0) {
+    if (i !== 0) {
       dateArray.push(today);
     }
   }
@@ -64,39 +60,14 @@ function getDateArray() {
 function getDateForAttr(date) {
   let day = date.getDate();
   let month = date.getMonth() + 1;
-  let yr = date.getFullYear();
+  let year = date.getFullYear();
   if (day < 10) {
     day = "0" + day;
   }
   if (month < 10) {
     month = "0" + month;
   }
-  return yr + "-" + month + "-" + day;
-}
-
-function getUserTaksFortheDay(userid, currentDate) {
-  //userid = Users[i].id;
-  let str = "";
-  let ustasks = userMap[userid];
-  let curdateSTR = getDateForAttr(currentDate);
-  if (ustasks.length !== 0) {
-    for (let i = 0; i < ustasks.length; i++) {
-      if (
-        ustasks[i].planStartDate <= curdateSTR &&
-        curdateSTR <= ustasks[i].planEndDate
-      ) {
-        let title =
-          "dates: " + ustasks[i].planStartDate + " - " + ustasks[i].planEndDate;
-        str +=
-          " " +
-          `<p title="${title}" class="hide">` +
-          ustasks[i].subject +
-          `</p>`;
-      }
-    }
-  }
-
-  return str;
+  return year + "-" + month + "-" + day;
 }
 
 function parseDate(date) {
@@ -118,57 +89,57 @@ function renderBacklog() {
   txtbox.id = "search";
   backlogdiv.appendChild(txtbox);
 
-  for (let i = 0; i < backlog.length; i++) {
+  for (let i = 0; i < BACKLOG.length; i++) {
     let taskdiv = document.createElement("div");
-    taskdiv.setAttribute("taskid", backlog[i].id);
+    taskdiv.setAttribute("taskid", BACKLOG[i].id);
     taskdiv.setAttribute("i", i);
     taskdiv.id = "task" + (i + 1);
-    taskdiv.innerHTML = backlog[i].subject;
+    taskdiv.innerHTML = BACKLOG[i].subject;
     taskdiv.draggable = true;
     taskdiv.setAttribute(
       "hint",
-      backlog[i].planStartDate + " - " + backlog[i].planEndDate
+      BACKLOG[i].planStartDate + " - " + BACKLOG[i].planEndDate
     );
 
     backlogdiv.appendChild(taskdiv);
-    dnd(taskdiv);
+    applyDragAndDrop(taskdiv);
   }
 }
 
-function afterMouseUPonTask(taskidx, useridx, date) {
-  backlog[taskidx].planStartDate = date;
-  let x = parseDate(date);
-  x.setDate(x.getDate() + backlog[taskidx].dayGap);
-  backlog[taskidx].planEndDate = getDateForAttr(x);
+function afterMouseUpOnTask(taskIndex, userIndex, date) {
+  BACKLOG[taskIndex].planStartDate = date;
+  let fixDate = parseDate(date);
+  fixDate.setDate(fixDate.getDate() + BACKLOG[taskIndex].dayGap);
+  BACKLOG[taskIndex].planEndDate = getDateForAttr(fixDate);
 
-  userMap[useridx].push(backlog[taskidx]);
-  backlog.splice(taskidx, 1);
+  USERMAP[userIndex].push(BACKLOG[taskIndex]);
+  BACKLOG.splice(taskIndex, 1);
   renderBacklog();
 
   createCal(weekCounter);
 }
 
-function afterMouseUPonUser(taskidx, useridx) {
-  userMap[useridx].push(backlog[taskidx]);
-  backlog.splice(taskidx, 1);
+function afterMouseUpOnUser(taskIndex, userIndex) {
+  USERMAP[userIndex].push(BACKLOG[taskIndex]);
+  BACKLOG.splice(taskIndex, 1);
   renderBacklog();
 
   createCal(weekCounter);
 }
 
-function dnd(ball) {
-  ball.onmousedown = function (event) {
-    taskDnD = ball.cloneNode(true);
-    document.body.append(taskDnD);
+function applyDragAndDrop(target) {
+  target.onmousedown = function (event) {
+    targetClone = target.cloneNode(true);
+    document.body.append(targetClone);
 
-    taskDnD.style.position = "absolute";
-    taskDnD.style.zIndex = 1000;
+    targetClone.style.position = "absolute";
+    targetClone.style.zIndex = 1000;
 
     moveAt(event.pageX, event.pageY);
 
     function moveAt(pageX, pageY) {
-      taskDnD.style.left = pageX - taskDnD.offsetWidth / 2 + "px";
-      taskDnD.style.top = pageY - taskDnD.offsetHeight / 2 + "px";
+      targetClone.style.left = pageX - targetClone.offsetWidth / 2 + "px";
+      targetClone.style.top = pageY - targetClone.offsetHeight / 2 + "px";
     }
 
     function onMouseMove(event) {
@@ -177,42 +148,41 @@ function dnd(ball) {
 
     document.addEventListener("mousemove", onMouseMove);
 
-    taskDnD.onmouseup = function (event) {
-      taskDnD.hidden = true;
+    targetClone.onmouseup = function (event) {
+      targetClone.hidden = true;
 
-      let drp = document.getElementsByClassName("hide");
-      for (let x = 0; x < drp.length; x++) {
-        drp[x].style.transform = "scale(0)";
+      let calnendarTasks = document.getElementsByClassName("hide");
+      for (let elem of calnendarTasks) {
+        elem.style.transform = "scale(0)";
       }
 
       let currentElem = document.elementFromPoint(event.clientX, event.clientY);
-      //taskDnD.hidden = false;
 
-      for (let x = 0; x < drp.length; x++) {
-        drp[x].style.transform = "scale(1)";
+      for (let elem of calnendarTasks) {
+        elem.style.transform = "scale(1)";
       }
 
       if (currentElem.className == "droppable") {
-        let taskidx = ball.getAttribute("i");
-        let useridx = currentElem.getAttribute("userid");
+        let taskIndex = target.getAttribute("i");
+        let userIndex = currentElem.getAttribute("userid");
         let date = currentElem.getAttribute("date");
 
-        afterMouseUPonTask(taskidx, useridx, date);
+        afterMouseUpOnTask(taskIndex, userIndex, date);
       } else if (currentElem.className == "droppableUSER") {
-        let useridx = currentElem.getAttribute("userid");
-        let taskidx = ball.getAttribute("i");
+        let userIndex = currentElem.getAttribute("userid");
+        let taskIndex = target.getAttribute("i");
 
-        afterMouseUPonUser(taskidx, useridx);
+        afterMouseUpOnUser(taskIndex, userIndex);
       }
-      taskDnD.remove();
+      targetClone.remove();
 
-      taskDnD.onmouseup = null;
+      targetClone.onmouseup = null;
     };
 
-    taskDnD.ondragstart = function () {
+    targetClone.ondragstart = function () {
       return false;
     };
-    ball.ondragstart = function () {
+    target.ondragstart = function () {
       return false;
     };
   };
@@ -220,14 +190,13 @@ function dnd(ball) {
 
 function renderButtons(calendarDiv) {
   let buttondiv = document.createElement("div");
-  buttondiv.id = "btns";
+  buttondiv.id = "buttons";
   calendarDiv.appendChild(buttondiv);
 
-  for (let i = 0; i < CalLen; i++) {
-    //let btn = document.getElementById("btns");
+  for (let i = 0; i < CALENDARSIZE; i++) {
     let newdiv = document.createElement("div");
 
-    if (i !== 1 && i !== CalLen - 1) {
+    if (i !== 1 && i !== CALENDARSIZE - 1) {
       newdiv.style.transform = "scale(0)";
     }
 
@@ -236,7 +205,7 @@ function renderButtons(calendarDiv) {
       newdiv.id = "left";
     }
 
-    if (i == CalLen - 1) {
+    if (i == CALENDARSIZE - 1) {
       newdiv.innerHTML = "RIGHT";
       newdiv.id = "right";
     }
@@ -244,25 +213,25 @@ function renderButtons(calendarDiv) {
   }
 
   let leftButton = document.getElementById("right");
-  leftButton.addEventListener("click", shiftCalFwd);
+  leftButton.addEventListener("click", shiftCalendarForwards);
 
   let rightButton = document.getElementById("left");
-  rightButton.addEventListener("click", shiftCalBckwd);
+  rightButton.addEventListener("click", shiftCalendarBackwards);
 }
 
 function createCal(weekCounter) {
-  let calendarDiv = document.getElementById("cc");
-  calendarDiv.innerHTML = ""; //чистим содержимое
+  let calendardiv = document.getElementById("calendar");
+  calendardiv.innerHTML = ""; //чистим содержимое
 
-  renderButtons(calendarDiv);
+  renderButtons(calendardiv);
 
   let div = document.createElement("div");
   div.id = "header";
-  calendarDiv.appendChild(div);
+  calendardiv.appendChild(div);
 
   let dateArray = [""];
 
-  for (let i = 0; i < CalLen; i++) {
+  for (let i = 0; i < CALENDARSIZE; i++) {
     let header = document.getElementById("header");
     let newdiv = document.createElement("div");
     let today = new Date();
@@ -277,23 +246,22 @@ function createCal(weekCounter) {
     header.appendChild(newdiv);
   }
 
-  for (let i = 0; i < Users.length; i++) {
+  for (let i = 0; i < USERS.length; i++) {
     let newdiv = document.createElement("div");
-    let uname = "user" + Users[i].id;
+    let uname = "user" + USERS[i].id;
     newdiv.id = uname;
-    calendarDiv.appendChild(newdiv);
+    calendardiv.appendChild(newdiv);
 
     let rowdiv = document.getElementById(uname);
-    for (let j = 0; j < CalLen; j++) {
+    for (let j = 0; j < CALENDARSIZE; j++) {
       let div = document.createElement("div");
-      div.id = "divfortask-" + j + "-" + i;
       div.setAttribute("userid", +i + 1);
       if (j !== 0) {
         div.setAttribute("date", getDateForAttr(dateArray[j]));
         div.className = "droppable";
-        renderTasksforUser(dateArray[j], Users[i].id, div);
+        renderTasksforUser(dateArray[j], USERS[i].id, div);
       } else {
-        div.innerHTML = Users[i].firstName;
+        div.innerHTML = USERS[i].firstName;
         div.className = "droppableUSER";
       }
       rowdiv.appendChild(div);
@@ -301,36 +269,31 @@ function createCal(weekCounter) {
   }
 }
 
-function renderTasksforUser(currentDate, userid, div) {
-  let ustasks = userMap[userid];
+function renderTasksforUser(currentDate, userID, target) {
+  let tasks = USERMAP[userID];
   let curdateSTR = getDateForAttr(currentDate);
-  if (ustasks.length !== 0) {
-    for (let i = 0; i < ustasks.length; i++) {
-      if (
-        ustasks[i].planStartDate <= curdateSTR &&
-        curdateSTR <= ustasks[i].planEndDate
-      ) {
-        let tsk = document.createElement("div");
-        tsk.innerHTML = ustasks[i].subject;
-        tsk.className = "hide";
-        tsk.title = ustasks[i].planStartDate + " - " + ustasks[i].planEndDate;
-        div.appendChild(tsk);
+  if (tasks.length !== 0) {
+    for (let task of tasks) {
+      if (task.planStartDate <= curdateSTR && curdateSTR <= task.planEndDate) {
+        let newtask = document.createElement("div");
+        newtask.innerHTML = task.subject;
+        newtask.className = "hide";
+        newtask.title = task.planStartDate + " - " + task.planEndDate;
+        target.appendChild(newtask);
       }
     }
   }
 }
 
 window.onload = function () {
-  for (let i = 0; i < Users.length; i++) {
-    userMap[Users[i].id] = [];
+  for (let user of USERS) {
+    USERMAP[user.id] = [];
   }
 
-  for (let i = 0; i < Tasks.length; i++) {
-    Tasks[i].dayGap =
-      (parseDate(Tasks[i].planEndDate) - parseDate(Tasks[i].planStartDate)) /
-      86400000;
-    let task = Tasks[i];
-    task.executor ? userMap[task.executor].push(task) : backlog.push(task);
+  for (let task of TASKS) {
+    task.dayGap =
+      (parseDate(task.planEndDate) - parseDate(task.planStartDate)) / 86400000;
+    task.executor ? USERMAP[task.executor].push(task) : BACKLOG.push(task);
   }
 
   renderBacklog();
@@ -338,8 +301,8 @@ window.onload = function () {
   createCal(weekCounter);
 
   let leftButton = document.getElementById("right");
-  leftButton.addEventListener("click", shiftCalFwd);
+  leftButton.addEventListener("click", shiftCalendarForwards);
 
   let rightButton = document.getElementById("left");
-  rightButton.addEventListener("click", shiftCalBckwd);
+  rightButton.addEventListener("click", shiftCalendarBackwards);
 };
